1f8ffb027781b997e3f6cff4b337ebaaf466fb65
duskngai <duskngai@gmail.com>
2024-02-29 01:58:19 +0800

    20240229. Modify to nerf.

--- coach/modeling/__init__.py
+++ coach/modeling/__init__.py
@@ -2,11 +2,3 @@ from .architecture import (
     MODEL_REGISTRY,
     build_model,
 )
-
-from .nerf.architecture import (
-    NeRF
-)
-
-from .nerf.scene import (
-    VanillaScene
-)
--- coach/modeling/architecture/__init__.py
+++ coach/modeling/architecture/__init__.py
@@ -1,3 +1,1 @@
 from .build import MODEL_REGISTRY, build_model
-
-from ..nerf.architecture.nerf import NeRF
